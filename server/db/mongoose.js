1307ed866df87bc406796c9d6d3450e5287fbe9e
Sofia Johansson <sofiajohansson@Sofias-MacBook-Air.local>
2018-08-14 18:27:24 +0200

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // sets up mongoose to use promises and not only callbacks
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
                 // connects to mLab MongoDB app through heroku
 
 module.exports = {
